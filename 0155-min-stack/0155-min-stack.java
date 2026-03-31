b77f132ad41b85d5260953a16b2af921f2b1f7d8
Shaik Bibi Jahera <jaherashaikbibi@gmail.com>
2026-03-31 12:17:01 +0530

    Time: 5 ms (84.93%), Space: 47.2 MB (21.18%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        // If minStack is empty, the first value is the min
+        // Otherwise, push the smaller of current val and current min
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
