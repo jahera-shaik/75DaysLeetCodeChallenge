d9f955ca12a9db99d3d3a43d3db638ff9912ea32
Shaik Bibi Jahera <jaherashaikbibi@gmail.com>
2026-04-01 09:13:51 +0530

    Time: 6 ms (91.11%), Space: 45.4 MB (37.55%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                // The second pop is the left operand, the first pop is the right
+                int b = stack.pop();
+                int a = stack.pop();
+                
+                switch (token) {
+                    case "+": stack.push(a + b); break;
+                    case "-": stack.push(a - b); break;
+                    case "*": stack.push(a * b); break;
+                    case "/": stack.push(a / b); break; // Truncates toward zero by default
+                }
+            } else {
+                // It's a number, push it
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isOperator(String s) {
+        return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/");
+    }
+}
