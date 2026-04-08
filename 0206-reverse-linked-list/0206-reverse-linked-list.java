13035c846b2ff1d4e9e35f2f52aee833356174b5
Shaik Bibi Jahera <jaherashaikbibi@gmail.com>
2026-04-08 20:57:00 +0530

    Time: 0 ms (100%), Space: 44.1 MB (87.24%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    // 1. Iterative Solution
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        
+        while (curr != null) {
+            ListNode nextTemp = curr.next; // Store next
+            curr.next = prev;              // Reverse pointer
+            prev = curr;                   // Move prev forward
+            curr = nextTemp;               // Move curr forward
+        }
+        
+        return prev;
+    }
+
+    // 2. Recursive Solution (Uncomment to use)
+    /*
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        
+        return newHead;
+    }
+    */
+}
